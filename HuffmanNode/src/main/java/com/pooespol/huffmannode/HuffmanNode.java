db8182f41cecd2ffb77165b6af56dab26f9f27dc
Josemiu <josemigueldelgadob@yahoo.com>
2024-07-28 21:51:58 -0500

    act

---
+++ HuffmanNode/src/main/java/com/pooespol/huffmannode/HuffmanNode.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pooespol.huffmannode;
+
+/**
+ *
+ * @author José Miguel
+ */
+public class HuffmanNode {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
